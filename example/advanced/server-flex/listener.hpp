bec0d7f329090096b942430ea431138817ce2ceb
Tal Lancaster <tal.lancaster@ciere.com>
2019-02-20 17:28:53 -0800

    WIP trying TDD server; doesn't do anything

--- example/advanced/server-flex/listener.hpp
+++ example/advanced/server-flex/listener.hpp
@@ -1,2 +1,86 @@
 
+#include "beast.hpp"
+
+// Accepts incoming connections and launches the sessions
+class listener : public std::enable_shared_from_this<listener>
+{
+public:
+    listener (net::io_context& ioc_,
+              tcp::endpoint endpoint_,
+              const std::shared_ptr <const std::string>& docRoot_)
+    : _acceptor {ioc_}
+    , _docRoot {docRoot_}
+    {
+        beast::error_code ec;
+        
+        // Open acceptor
+        _acceptor.open (endpoint_.protocol(), ec);
+        if (ec) {
+            fail (ec, "open");
+            
+            return;
+        }
+        
+        // Allow address reuse
+        _acceptor.set_option(net::socket_base::reuse_address(true), ec);
+        if(ec)
+        {
+            fail(ec, "set_option");
+            return;
+        }
+        
+        // Bind to the server address
+        _acceptor.bind(endpoint_, ec);
+        if(ec)
+        {
+            fail(ec, "bind");
+            return;
+        }
+        
+        // Start listening for connections
+        _acceptor.listen(
+                         net::socket_base::max_listen_connections, ec);
+        if(ec)
+        {
+            fail(ec, "listen");
+            return;
+        }
+    }
+    
+    // Start accepting incoming connections
+    void run ()
+    {
+        assert (_acceptor.is_open() && "listener::run acceptor not open");
+        if (!_acceptor.is_open () ) {
+            return;
+        }
+        doAccept ();
+    }
+    
+    void doAccept ()
+    {
+        _acceptor.async_accept (
+            beast::bind_front_handler (
+                &listener::onAccept,
+                shared_from_this()  ) );
+    }
+    
+    void onAccept (beast::error_code ec_, tcp::socket socket_)
+    {
+        if (ec_) {
+            fail (ec_, "accept");
+        }
+        
+        else {
+            // Create the detector http_session and run it
+        }
+        
+        // Accept another connection
+        
+    }
+    
+private:
+    tcp::acceptor _acceptor;
+    std::shared_ptr<const std::string> _docRoot;
+};
 
---
+++ example/advanced/server-flex/test1.cpp
@@ -0,0 +1,161 @@
+
+#include <algorithm>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "beast.hpp"
+#include "listener.hpp"
+
+#include <catch2/catch.hpp>
+
+// setup namespaces
+namespace beast = boost::beast;                 // from <boost/beast.hpp>
+namespace http = beast::http;                   // from <boost/beast/http.hpp>
+namespace websocket = beast::websocket;         // from <boost/beast/websocket.hpp>
+namespace net = boost::asio;                    // from <boost/asio.hpp>
+namespace ssl = net::ssl;               // from <boost/asio/ssl.hpp>
+using tcp = net::ip::tcp;               // from <boost/asio/ip/tcp.hpp>
+
+constexpr auto portStr {"8080"};
+constexpr auto port {static_cast<unsigned int> (8080) };
+constexpr auto docRootSrc {"/Users/tal/Desktop/other/rmr/public_html"};
+constexpr auto target {"simple.html"};
+constexpr auto localhost {"127.0.0.1"};
+constexpr auto threads {1};
+const auto address {net::ip::make_address(localhost) };
+const auto docRoot {std::make_shared <std::string> (docRootSrc) };
+
+// Report a failure
+void fail(beast::error_code ec, char const* what)
+{
+    std::cerr << what << ": " << ec.message() << "\n";
+}
+
+// may need to be changed
+using streamType = beast::tcp_stream<net::io_context::executor_type>;
+
+class Client : public std::enable_shared_from_this<Client>
+{
+    
+public:
+    Client (net::io_context& ioc_)
+    : _resolver {ioc_}
+    , _stream {ioc_}
+    { }
+    
+    void run (const char* host_,
+              const char* port_,
+              const char* target_,
+              int version_)
+    {
+        _req.version (version_);
+        _req.method(http::verb::get);
+        _req.target(target_);
+        _req.set(http::field::host, host_);
+        _req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+        
+        _resolver.async_resolve(host_, port_, beast::bind_front_handler(&Client::onResolve, shared_from_this() ) );
+    }
+    
+    void onResolve (beast::error_code ec_,
+                    tcp::resolver::results_type results_)
+    {
+        if (ec_) {
+            return fail (ec_, "resolve");
+        }
+        
+        // set timeout
+        _stream.expires_after(std::chrono::seconds (5) );
+        
+        // Make connection on the IP address
+        beast::async_connect(_stream, results_,
+                             beast::bind_front_handler(&Client::onConnect,
+                                                       shared_from_this()) );
+    }
+    
+    void onConnect (beast::error_code ec_, tcp::resolver::results_type::endpoint_type)
+    {
+        if (ec_) {
+            return fail (ec_, "connect");
+        }
+        
+        // Set timeout on operation
+        _stream.expires_after(std::chrono::seconds (5) );
+        
+        // Send HTTP request to host
+        http::async_write(_stream, _req,
+                          beast::bind_front_handler(&Client::onWrite,
+                                                    shared_from_this()  ) );
+    }
+    
+    void onWrite(beast::error_code ec_,
+                 std::size_t bytesTransferred_)
+    {
+        boost::ignore_unused(bytesTransferred_);
+        
+        if (ec_) {
+            return fail (ec_, "write");
+        }
+        
+        // Receive HTTP response
+        http::async_read(_stream, _buffer, _res,
+                         beast::bind_front_handler(&Client::onRead,
+                                                   shared_from_this() ) );
+    }
+    
+    void onRead (beast::error_code ec_,
+                 std::size_t bytesTransferred_)
+    {
+        boost::ignore_unused(bytesTransferred_);
+        
+        if (ec_) {
+            return fail (ec_, "read");
+        }
+        
+        std::cout << _res << "\n";
+        
+        // gracefully close the socket
+        _stream.socket().shutdown(tcp::socket::shutdown_both, ec_);
+        
+        // not connected
+        if (ec_ && ec_ != beast::errc::not_connected) {
+            return fail (ec_, "shutdown");
+        }
+        
+        // connection closed gracefully
+    }
+
+private:
+    tcp::resolver _resolver;
+    streamType _stream;
+    beast::flat_buffer _buffer;
+    http::request<http::empty_body> _req;
+    http::response<http::string_body> _res;
+};
+
+TEST_CASE("Simple server")
+{
+    SECTION("setup")
+    {
+        net::io_context ioc {threads};
+        std::make_shared <listener> (ioc,
+                                     tcp::endpoint {address, port},
+                                     docRoot)->run();
+        
+        // Capture SIGINT and SIGTERM
+        
+        // Run the I/O service on the requested number of threads
+        // forcing to 1 thread (at lest for now ...
+        
+        std::make_shared<Client>(ioc)->run (localhost, portStr, target, 11/* version*/);
+        ioc.run();
+        
+    }
+}
+    
+
